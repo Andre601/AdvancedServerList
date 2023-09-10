8bdaa232ca16444ad5ac98a8201b974c1799fc00
Andre601 <github@andre601.ch>
2023-09-11 00:12:27 +0200

    Use Promis.all([...])

--- docs/assets/js/repo-stats.js
+++ docs/assets/js/repo-stats.js
@@ -1,19 +1,16 @@
 document$.subscribe(async () => {
     const url = 'https://codeberg-stats-proxy.vercel.app/repo'
     
-    const release_data = await fetch(`${url}/latest-release`)
-        .then(_ => _.json());
-    const repo_data = await fetch(`${url}/stats`)
-        .then(_ => _.json());
-    
-    console.log(release_data);
-    console.log(repo_data);
+    const [release, repo] = await Promise.all([
+        fetch(`${url}/latest-release`).then(_ => _.json()),
+        fetch(`${url}/stats`).then(_ => _.json()),
+    ]);
     
     const list = document.querySelectorAll('ul[data-md-component="codeberg-stats"]');
     
     list.forEach((entry) => {
-        entry.innerHTML = `<li class="md-source__fact md-source__fact--version">${release_data.name}</li>
-            <li class="md-source__fact md-source__fact--stars">${repo_data.stars}</li>
-            <li class="md-source__fact md-source__fact--forks">${repo_data.forks}</li>`
+        entry.innerHTML = `<li class="md-source__fact md-source__fact--version">${release.name}</li>
+            <li class="md-source__fact md-source__fact--stars">${repo.stars}</li>
+            <li class="md-source__fact md-source__fact--forks">${repo.forks}</li>`
     })
 })
