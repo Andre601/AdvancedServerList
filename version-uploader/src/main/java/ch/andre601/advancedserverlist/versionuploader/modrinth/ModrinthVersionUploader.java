01611d41300aa8b0ccf9952241a7dc39e5c11332
Andre601 <github@andre601.ch>
2023-09-21 20:59:35 +0200

    Add 1.20.2 for ModrinthVersionUploader

--- version-uploader/src/main/java/ch/andre601/advancedserverlist/versionuploader/modrinth/ModrinthVersionUploader.java
+++ version-uploader/src/main/java/ch/andre601/advancedserverlist/versionuploader/modrinth/ModrinthVersionUploader.java
@@ -48,7 +48,7 @@ public class ModrinthVersionUploader{
         PlatformInfo.BUNGEECORD,
         PlatformInfo.VELOCITY
     );
-    private final List<String> versions = List.of("1.19.4", "1.20", "1.20.1");
+    private final List<String> versions = List.of("1.19.4", "1.20", "1.20.1", "1.20.2");
     
     private final ModrinthAPI api;
     
