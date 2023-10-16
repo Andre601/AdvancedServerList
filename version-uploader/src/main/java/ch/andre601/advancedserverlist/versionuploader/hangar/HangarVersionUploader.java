17e9692028ac45846bdfd09efc91d3132154d337
Andre601 <github@andre601.ch>
2023-10-16 11:22:37 +0200

    Remove unused import

--- version-uploader/src/main/java/ch/andre601/advancedserverlist/versionuploader/hangar/HangarVersionUploader.java
+++ version-uploader/src/main/java/ch/andre601/advancedserverlist/versionuploader/hangar/HangarVersionUploader.java
@@ -38,7 +38,6 @@ import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.FileBody;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.entity.mime.StringBody;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.HttpMessage;
